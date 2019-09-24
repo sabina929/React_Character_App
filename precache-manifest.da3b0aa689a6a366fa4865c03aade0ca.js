b2a011014808d6e4425884e37850b7506f84eda2
sabina929 <sabina.abbasova2909@gmail.com>
2019-09-24 19:27:20 +0400

    Updates

---
+++ precache-manifest.da3b0aa689a6a366fa4865c03aade0ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e32cdcf4e1124448586e64989c11025",
+    "url": "/React_Character_App/index.html"
+  },
+  {
+    "revision": "f0c5380ed0a0ae1ce584",
+    "url": "/React_Character_App/static/css/main.b229ec99.chunk.css"
+  },
+  {
+    "revision": "c38a6685875724ca66ef",
+    "url": "/React_Character_App/static/js/2.867acb1a.chunk.js"
+  },
+  {
+    "revision": "f0c5380ed0a0ae1ce584",
+    "url": "/React_Character_App/static/js/main.c579d223.chunk.js"
+  },
+  {
+    "revision": "bcfcc1773ea5da2c67d4",
+    "url": "/React_Character_App/static/js/runtime~main.371961c4.js"
+  }
+]);
